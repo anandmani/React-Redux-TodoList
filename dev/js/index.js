39f199405be296355a19bbf124e2c6d0d27de9c9
Anand Mani <cma.camelot@gmail.com>
2016-10-20 16:27:04 +0530

    Adding comment about provider

--- dev/js/index.js
+++ dev/js/index.js
@@ -8,6 +8,8 @@ import allReducers from "./reducers/allReducers.js";
 
 const store = createStore(allReducers);
 
+//Note: we have not used Provider here as a wrapper to App. Therefore, store is only visible in App. If App had children components, we need to pass store as a prop again. Instead use Provider.
+//Note: Provider will not provider Store as a prop. But it will enable us to use mapStateToProps and get state value.
 ReactDOM.render(
     <App store = {store}/>,
     document.getElementById('root')
